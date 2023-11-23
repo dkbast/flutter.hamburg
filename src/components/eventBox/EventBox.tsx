b0afb11f59c0b506bf156e65a1f2dc55be920914
Max Berndt <mb@maxberndt.com>
2023-11-23 15:16:21 +0100

    finish event box

--- src/components/eventBox/EventBox.tsx
+++ src/components/eventBox/EventBox.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./EventBox.module.css";
+import locationIcon from './location.svg'
+import externalLinkIcon from './external.svg'
 
 export const EventBox: React.FC<unknown> = () => {
   return (
@@ -10,6 +12,12 @@ export const EventBox: React.FC<unknown> = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
+        <div className={styles.status}>Free</div>
+        <a href={""} className={styles.location}>
+            <img src={locationIcon}></img>
+            Netlight Consulting GmbH, Große Bleichen 23-27
+            <img src={externalLinkIcon}></img>
+        </a>
       </section>
       <div className={styles.calendarIcon}>
         <div className={styles.month}>NOV</div>
