0826c1b63054b276e11dcfece68abd0587aaec36
Max Berndt <mb@maxberndt.com>
2023-12-15 22:21:03 +0100

    make events configurable in events.json

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
 import express from "express";
-// import pug from "pug";
+import events from "./events.json";
 
 const app = express();
 const port = 3000;
 app.set("view engine", "pug");
 app.use(express.static("public"));
 
+const featured = events.find((e) => e.isFeatured);
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { featured });
 });
 
 app.listen(port, () => {
